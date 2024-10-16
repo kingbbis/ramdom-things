449fcf9fa05b8510b6246090f93456f60e01b500
King Li <106876369+kingbbis@users.noreply.github.com>
2024-10-16 20:05:10 +0800

    Add files via upload

---
+++ 1.bat
@@ -0,0 +1,4 @@
+@echo off
+echo The command prompt has been disabled by your administrator.
+pause
+exit
---
+++ 2.bat
@@ -0,0 +1,25 @@
+@echo off
+color 37
+title  Batch file commands demonstration
+echo Batch file commands
+echo 1. @echo off/on
+echo Usage is to turn on or off about showing who is running the command 
+pause
+echo 2. echo _____
+echo Usage is to show what is behind is echo
+pause
+echo 3. Pause
+echo Usage is to stop after the previous command 
+pause
+echo 4. exit
+echo Usage is to leave the app
+pause
+echo 5. cls
+echo Usage is to clear the screen 
+pause
+echo 6. Timeout “seconds “
+echo Usage is to do for how many seconds you want
+pause
+echo This is all I know. lol 
+pause
+exit
